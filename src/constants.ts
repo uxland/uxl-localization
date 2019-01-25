57f8cba1ab08f4347f2a538782f67f26173161de
danielcabiscol <danielcabiscol@gmail.com>
2019-01-25 13:10:57 +0100

    perf(): update lit-element 2.0
    BREAKING CHANGE: update lit-element 2.0 and improve some features

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,9 @@
 import { actionNameBuilder } from "@uxland/uxl-redux/action-name-builder";
 const prefix = "uxl-localization";
+
 export const locActionNamesFactory = (action: string) => {
     const actionsBuilder = actionNameBuilder(prefix);
     return actionsBuilder(action);
 };
+
 export default locActionNamesFactory;
